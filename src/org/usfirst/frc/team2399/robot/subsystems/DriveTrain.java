b7f851bba77634e2846d94ef8aaa98f3d112637f
aliu19 <catherineliu@Andrews-MacBook-Pro-2.local>
2017-01-28 17:31:08 -0500

    Set up the P-Loop code for driving at an angle. Added methods in DriveTrain to implement in JoyDrive, and possibly an ANGLE_P_CONSTANT in RobotMap.

--- src/org/usfirst/frc/team2399/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2399/robot/subsystems/DriveTrain.java
@@ -19,6 +19,8 @@ public class DriveTrain extends Subsystem {
 	private double time;
 	private Timer timer = new Timer();
 	private double distancePConstant = RobotMap.DISTANCE_P_CONSTANT;
+	private double anglePConstant = RobotMap.ANGLE_P_CONSTANT;
+	private double desiredAngle;
 	
 	public DriveTrain() {
 		leftFrontTalon = new CANTalon(RobotMap.DRIVETRAIN_LEFT_TALON_FRONT_ADDRESS);
@@ -133,15 +135,15 @@ public class DriveTrain extends Subsystem {
 	 * Methods to increment, decrement, and get the angle constant
 	 */
 	
-	public void incrementAngleConstant(){
+	public void incrementDistanceConstant(){
 		distancePConstant += RobotMap.DISTANCE_INCREMENT_CONSTANT;
 	}
 	
-	public void decrementAngleConstant(){
+	public void decrementDistanceConstant(){
 		distancePConstant -= RobotMap.DISTANCE_DECREMENENT_CONSTANT;
 	}
 	
-	public double getAngleConstant(){
+	public double getDistanceConstant(){
 		return distancePConstant;
 	}
 	
@@ -149,5 +151,30 @@ public class DriveTrain extends Subsystem {
 	protected void initDefaultCommand() {
 		setDefaultCommand(new JoyDrive());
 	}
-
+	
+	/**
+	 * Sets desired angle for driving at an angle
+	 * @param goalAngle
+	 */
+	public void setDesiredAngle(double goalAngle) {
+		desiredAngle = goalAngle;
+	}
+	
+	/**
+	 * Uses the encoder to get the angle
+	 * @return: the angle
+	 *
+	public double getCurrentAngle() {
+		return //TODO: talon code
+	}
+	*/
+	
+	/**
+	 * P loop for driving to a specified angle
+	 */	
+	public void moveToAngle() {
+		//TODO: p loop from getCurrentAngle()
+	}
+	
+		
 }
