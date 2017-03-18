2611cbaefa69eca9251ab179d0b19709fdb2f81a
laurenegts <lauren.egts@gmail.com>
2017-03-18 12:12:18 -0400

    deleted the driving backwards and opening the gear collector from the center lift auto

--- src/org/usfirst/frc/team2399/robot/commands/AutoDriveToCenterLift.java
+++ src/org/usfirst/frc/team2399/robot/commands/AutoDriveToCenterLift.java
@@ -9,13 +9,9 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
  * Drive to the center lift, place gear, drive away from the lift
  * Keeps the Gear Collector open for about 3 seconds before closing it
  */
-public class AutoDriveToCenterLift extends CommandGroup {;
+public class AutoDriveToCenterLift extends CommandGroup {
 	
     public AutoDriveToCenterLift() {
-    	addSequential(new DriveDistanceHoldAngle(69, 40, 2.25));
-    	addSequential(new GearCollect(!RobotMap.GEAR_SOLENOID_IN,RobotMap.GEAR_SOLENOID_OUT));
-    	addSequential(new WaitCommand(3));
-    	addParallel(new GearCollect(RobotMap.GEAR_SOLENOID_IN,!RobotMap.GEAR_SOLENOID_OUT));
-    	addParallel(new DriveDistanceHoldAngle(-20, 40, 1.25));
+    	addSequential(new DriveDistanceHoldAngle(69, 40, 3));
     }
 }
