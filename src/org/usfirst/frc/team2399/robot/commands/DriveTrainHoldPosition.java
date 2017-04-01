768ed6dee8a9f642ceaf120d1e97c1a5e6b8e745
laurenegts <lauren.egts@gmail.com>
2017-03-31 20:57:34 -0400

    created new command/button to set the speed to 0 so the robot doesn't move

---
+++ src/org/usfirst/frc/team2399/robot/commands/DriveTrainHoldPosition.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team2399.robot.commands;
+
+import org.usfirst.frc.team2399.robot.Robot;
+import org.usfirst.frc.team2399.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveTrainHoldPosition extends Command {
+
+	private DriveTrain driveTrain = Robot.driveTrain;
+    public DriveTrainHoldPosition() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(driveTrain);
+    	driveTrain.setSpeedControlMode();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	driveTrain.driveLeftVelocity(0);
+    	driveTrain.driveRightVelocity(0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
